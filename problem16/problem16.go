3cb6475636c05f1936d76a8e360c05e18ed11654
alessiosavi <alessiosavibtc@gmail.com>
2019-10-31 21:44:55 +0100

    Solution to problem 16

--- problem16/problem16.go
+++ problem16/problem16.go
@@ -1,1 +1,22 @@
 package problem16
+
+import (
+	"fmt"
+	"log"
+	"math"
+	"strconv"
+)
+
+func Problem16(n float64) {
+
+	number := math.Pow(2, n)
+	log.Println(number)
+	m := fmt.Sprintf("%f", number)
+	sum := 0
+	for i := range m {
+		data, _ := strconv.Atoi(string(m[i]))
+		sum += data
+	}
+	log.Println(sum)
+
+}
---
+++ problem16/problem16_test.go
@@ -0,0 +1,7 @@
+package problem16
+
+import "testing"
+
+func TestProblem16(t *testing.T) {
+	Problem16(1000)
+}
